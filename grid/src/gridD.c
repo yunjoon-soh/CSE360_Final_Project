f54a991e2ebc0219cee36db10f113c773627eb58
Alexa Rockwell <alexa.rockwell@stonybrook.edu>
2017-05-17 19:01:17 -0700

    added cases for open, close, read, and write

--- grid/src/gridD.c
+++ grid/src/gridD.c
@@ -136,7 +136,22 @@ int main(int argc, char** argv) {
 			orig_eax = ptrace(PTRACE_PEEKUSER, childPid, 4 * ORIG_EAX, NULL);
 			DEBUG(2, "Intercepted %ld\n", orig_eax);
 
-			if (orig_eax == SYS_write) {
+			if (orig_eax == SYS_read ) {
+				if (insyscall == 0) {
+					/* Syscall entry */
+					insyscall = 1;
+					params[0] = ptrace(PTRACE_PEEKUSER, childPid, 4 * EBX, NULL);
+					params[1] = ptrace(PTRACE_PEEKUSER, childPid, 4 * ECX, NULL);
+					params[2] = ptrace(PTRACE_PEEKUSER, childPid, 4 * EDX, NULL);
+					DEBUG(1, "Read called with %ld, %ld, %ld\n", params[0], params[1], params[2]);
+				}
+				else { /* Syscall exit */
+					eax = ptrace(PTRACE_PEEKUSER, childPid, 4 * EAX, NULL);
+					DEBUG(2, "Read returned with %ld\n", eax);
+					insyscall = 0;
+				}
+			}
+			else if (orig_eax == SYS_write ) {
 				if (insyscall == 0) {
 					/* Syscall entry */
 					insyscall = 1;
@@ -151,10 +166,40 @@ int main(int argc, char** argv) {
 					insyscall = 0;
 				}
 			}
+			else if (orig_eax == SYS_open ) {
+				if (insyscall == 0) {
+					/* Syscall entry */
+					insyscall = 1;
+					params[0] = ptrace(PTRACE_PEEKUSER, childPid, 4 * EBX, NULL);
+					params[1] = ptrace(PTRACE_PEEKUSER, childPid, 4 * ECX, NULL);
+					params[2] = ptrace(PTRACE_PEEKUSER, childPid, 4 * EDX, NULL);
+					DEBUG(1, "Open called with %ld, %ld, %ld\n", params[0], params[1], params[2]);
+				}
+				else { /* Syscall exit */
+					eax = ptrace(PTRACE_PEEKUSER, childPid, 4 * EAX, NULL);
+					DEBUG(2, "Open returned with %ld\n", eax);
+					insyscall = 0;
+				}
+			}
+			else if (orig_eax == SYS_close ) {
+				if (insyscall == 0) {
+					/* Syscall entry */
+					insyscall = 1;
+					params[0] = ptrace(PTRACE_PEEKUSER, childPid, 4 * EBX, NULL);
+					params[1] = ptrace(PTRACE_PEEKUSER, childPid, 4 * ECX, NULL);
+					params[2] = ptrace(PTRACE_PEEKUSER, childPid, 4 * EDX, NULL);
+					DEBUG(1, "Close called with %ld, %ld, %ld\n", params[0], params[1], params[2]);
+				}
+				else { /* Syscall exit */
+					eax = ptrace(PTRACE_PEEKUSER, childPid, 4 * EAX, NULL);
+					DEBUG(2, "Close returned with %ld\n", eax);
+					insyscall = 0;
+				}
+			}
 			else {
 				DEBUG(1, "Child made syscall #%ld\n", orig_eax);
 			}
-			ptrace(PTRACE_SYSCALL, childPid, NULL, NULL);
+			ptrace(PTRACE_SYSCALL, orig_eax == SYS_writechildPid, NULL, NULL);
 
 			// 0. Intercept Syscall and libcalls
 			// 1. Communicates with the Snoopy's program
